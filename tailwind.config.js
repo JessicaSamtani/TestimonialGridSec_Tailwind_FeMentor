73b7fe303ca65b8aed36ac2cf4d53a936a02b3c7
Jessica Samtani <jessicasamtani@gmail.com>
2023-10-12 16:55:36 +0700

    feat: added the missing files and recommit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        moderateViolate: "hsl(263, 55%, 52%)",
+        veryDarkGrayishBlue: "hsl(217, 19%, 35%)",
+        veryDarkBlackishBlue: "hsl(219, 29%, 14%)",
+        lightGray: "hsl(0, 0%, 81%)",
+        lightGrayishBlue: "hsl(210, 46%, 95%)",
+      },
+      fontFamily: {
+        barlowSemiCond: ["Barlow Semi Condensed", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
